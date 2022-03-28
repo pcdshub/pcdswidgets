be1bea4ae9af4777fc4617661bf600ba51372fff
Zachary Lentz <zlentz@slac.stanford.edu>
2022-03-28 15:36:47 -0700

    ENH: add quick tester script to check widget on live device

---
+++ pcdswidgets/vacuum/demo/__main__.py
@@ -0,0 +1,22 @@
+"""
+Show a fully functional widget. Useful for development.
+
+Invoke as e.g.
+"python -m pcdswidgets.vacuum.demo PneumaticValveDA CRIX:VGC:11"
+"""
+import sys
+
+from qtpy.QtWidgets import QApplication
+
+from ..gauges import * # noqa
+from ..others import * # noqa
+from ..pumps import * # noqa
+from ..valves import * # noqa
+
+
+cls = sys.argv[1]
+app = QApplication([])
+widget = globals()[cls]()
+widget.channels_prefix = sys.argv[2]
+widget.show()
+app.exec()
