5555e009be1dc0e1699a20ca41b2dd7b2b97c9e3
Ken Lauer <klauer@slac.stanford.edu>
2022-06-08 16:09:21 -0700

    WIP/ENH: designer widgets via entrypoints

--- setup.py
+++ setup.py
@@ -12,12 +12,43 @@ if len(git_requirements) > 0:
           "\n".join(f' {r}' for r in git_requirements) +
           "\n\nmanually")
 
-setup(name='pcdswidgets',
-      author='SLAC National Accelerator Laboratory',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
-      packages=find_packages(),
-      include_package_data=True,
-      install_requires=requirements,
-      description='LCLS PyDM Widget Library',
-      )
+# Widgets to be displayed in the Qt Designer:
+designer_widgets = [
+    "SymbolBase=pcdswidgets.vacuum.base:PCDSSymbolBase",
+    "FilterSortWidgetTable=pcdswidgets.table:FilterSortWidgetTable",
+    "PneumaticValve=pcdswidgets.vacuum.valves:PneumaticValve",
+    "PneumaticValveNO=pcdswidgets.vacuum.valves:PneumaticValveNO",
+    "PneumaticValveDA=pcdswidgets.vacuum.valves:PneumaticValveDA",
+    "ApertureValve=pcdswidgets.vacuum.valves:ApertureValve",
+    "FastShutter=pcdswidgets.vacuum.valves:FastShutter",
+    "NeedleValve=pcdswidgets.vacuum.valves:NeedleValve",
+    "ProportionalValve=pcdswidgets.vacuum.valves:ProportionalValve",
+    "RightAngleManualValve=pcdswidgets.vacuum.valves:RightAngleManualValve",
+    "ControlValve=pcdswidgets.vacuum.valves:ControlValve",
+    "ControlOnlyValveNC=pcdswidgets.vacuum.valves:ControlOnlyValveNC",
+    "ControlOnlyValveNO=pcdswidgets.vacuum.valves:ControlOnlyValveNO",
+
+    "IonPump=pcdswidgets.vacuum.pumps:IonPump",
+    "TurboPump=pcdswidgets.vacuum.pumps:TurboPump",
+    "ScrollPump=pcdswidgets.vacuum.pumps:ScrollPump",
+    "GetterPump=pcdswidgets.vacuum.pumps:GetterPump",
+
+    "RoughGauge=pcdswidgets.vacuum.gauges:RoughGauge",
+    "HotCathodeGauge=pcdswidgets.vacuum.gauges:HotCathodeGauge",
+    "ColdCathodeGauge=pcdswidgets.vacuum.gauges:ColdCathodeGauge",
+    "RGA=pcdswidgets.vacuum.others:RGA",
+]
+
+setup(
+    name="pcdswidgets",
+    author="SLAC National Accelerator Laboratory",
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=requirements,
+    description="LCLS PyDM Widget Library",
+    entry_points={
+        "pydm.widget": designer_widgets,
+    }
+)
