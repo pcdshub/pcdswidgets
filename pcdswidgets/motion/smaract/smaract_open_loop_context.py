c3f8e493d7c8b536ab18b48a57d4321e82e8bc21
aberges-SLAC <aberges@slac.stanford.edu>
2026-04-23 11:14:29 -0700

    ENH: add SmarAct open-loop context screen

---
+++ pcdswidgets/motion/smaract/smaract_open_loop_context.py
@@ -0,0 +1,39 @@
+"""
+Originally generated from jinja template ui_main_widget.j2
+
+This file can be safely edited to change the runtime behavior of the widget.
+"""
+import logging
+
+from pcdswidgets.builder.designer_options import DesignerOptions
+from pcdswidgets.builder.icon_options import IconOptions
+from pcdswidgets.generated.motion.smaract.smaract_open_loop_context_base import SmaractOpenLoopContextBase
+from qtpy.QtWidgets import QWidget
+from pydm.widgets import PyDMPushButton
+from pydm import Display
+
+logger = logging.getLogger(__name__)
+
+class SmaractOpenLoopContext(SmaractOpenLoopContextBase):
+    clear_step_count: PyDMPushButton
+
+    designer_options = DesignerOptions(
+        group="ECS Motion Smaract",
+        is_container=False,
+        icon=IconOptions.NONE,
+    )
+
+
+class SmarActOpenLoopContextDisplay(SmaractOpenLoopContext, Display):
+    """This widget is intended to be opened with a PyDMRelatedDisplayButton"""
+    def __init__(self, parent: QWidget | None = None, args=None, macros=None):
+        # Initialize Display FIRST with macros, mro is mean
+        Display.__init__(self, parent=parent, args=args, macros=macros)
+        # Then initialize DesignerWidget
+        SmaractOpenLoopContext.__init__(self, parent=parent)
+        
+        # Sync Display's macros into DesignerWidget's macro system
+        # Otherwise PyDMRelatedDisplayButtons will not pass their macros
+        if macros:
+            for key, value in macros.items():
+                self._set_macro(key, value)
