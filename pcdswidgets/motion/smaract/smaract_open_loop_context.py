997031b0244eadd54add33d58ff465cc799aad06
aberges-SLAC <aberges@slac.stanford.edu>
2026-04-23 13:28:19 -0700

    STY: pre-commit fix

--- pcdswidgets/motion/smaract/smaract_open_loop_context.py
+++ pcdswidgets/motion/smaract/smaract_open_loop_context.py
@@ -3,16 +3,19 @@ Originally generated from jinja template ui_main_widget.j2
 
 This file can be safely edited to change the runtime behavior of the widget.
 """
+
 import logging
+
+from pydm import Display
+from pydm.widgets import PyDMPushButton
+from qtpy.QtWidgets import QWidget
 
 from pcdswidgets.builder.designer_options import DesignerOptions
 from pcdswidgets.builder.icon_options import IconOptions
 from pcdswidgets.generated.motion.smaract.smaract_open_loop_context_base import SmaractOpenLoopContextBase
-from qtpy.QtWidgets import QWidget
-from pydm.widgets import PyDMPushButton
-from pydm import Display
 
 logger = logging.getLogger(__name__)
+
 
 class SmaractOpenLoopContext(SmaractOpenLoopContextBase):
     clear_step_count: PyDMPushButton
@@ -26,12 +29,13 @@ class SmaractOpenLoopContext(SmaractOpenLoopContextBase):
 
 class SmarActOpenLoopContextDisplay(SmaractOpenLoopContext, Display):
     """This widget is intended to be opened with a PyDMRelatedDisplayButton"""
+
     def __init__(self, parent: QWidget | None = None, args=None, macros=None):
         # Initialize Display FIRST with macros, mro is mean
         Display.__init__(self, parent=parent, args=args, macros=macros)
         # Then initialize DesignerWidget
         SmaractOpenLoopContext.__init__(self, parent=parent)
-        
+
         # Sync Display's macros into DesignerWidget's macro system
         # Otherwise PyDMRelatedDisplayButtons will not pass their macros
         if macros:
--- pcdswidgets/motion/smaract/smaract_tip_tilt_double.py
+++ pcdswidgets/motion/smaract/smaract_tip_tilt_double.py
@@ -3,19 +3,22 @@ Originally generated from jinja template ui_main_widget.j2
 
 This file can be safely edited to change the runtime behavior of the widget.
 """
-import logging
-import json
 
+import json
+import logging
 from pathlib import Path
-from pcdswidgets.builder.designer_options import DesignerOptions
-from pcdswidgets.builder.icon_options import IconOptions
-from pcdswidgets.generated.motion.smaract.smaract_tip_tilt_double_base import SmaractTipTiltDoubleBase
+
 from pydm.widgets import PyDMPushButton, PyDMRelatedDisplayButton
 from qtpy.QtCore import QTimer
 from qtpy.QtGui import QIcon
-from qtpy.QtWidgets import QWidget, QCheckBox
+from qtpy.QtWidgets import QCheckBox, QWidget
+
+from pcdswidgets.builder.designer_options import DesignerOptions
+from pcdswidgets.builder.icon_options import IconOptions
+from pcdswidgets.generated.motion.smaract.smaract_tip_tilt_double_base import SmaractTipTiltDoubleBase
 
 logger = logging.getLogger(__name__)
+
 
 class SmaractTipTiltDouble(SmaractTipTiltDoubleBase):
     # some type hinting
@@ -40,27 +43,27 @@ class SmaractTipTiltDouble(SmaractTipTiltDoubleBase):
         self.horizontal_invert.stateChanged.connect(self._invert_horizontal)
         # For now, we will link the expert screens for tip-tilts
         # to a limited context window
-        self.vertical_expert_screen.setFilenames([str(Path(__file__).parent / 'smaract_open_loop_context.py')])
-        self.horizontal_expert_screen.setFilenames([str(Path(__file__).parent / 'smaract_open_loop_context.py')])
+        self.vertical_expert_screen.setFilenames([str(Path(__file__).parent / "smaract_open_loop_context.py")])
+        self.horizontal_expert_screen.setFilenames([str(Path(__file__).parent / "smaract_open_loop_context.py")])
         self._macros_timer = QTimer(parent=self)
         self._macros_timer.timeout.connect(self._setup_expert_screens)
         self._macros_timer.setInterval(100)
         self._macros_timer.setSingleShot(True)
         self._macros_timer.start()
-    
+
     @staticmethod
     def get_designer_icon() -> str:
         """Icon for usage in Qt designer."""
-        return QIcon(str(Path(__file__).parent / 'smaract_tip_tilt_qt_icon.png'))
+        return QIcon(str(Path(__file__).parent / "smaract_tip_tilt_qt_icon.png"))
 
     def _setup_expert_screens(self):
         """Macros aren't immediately available through _get_macros, wait until they are."""
-        if not self._get_macro('vertical_motor') or not self._get_macro('horizontal_motor'):
+        if not self._get_macro("vertical_motor") or not self._get_macro("horizontal_motor"):
             self._macros_timer.start()
             return
-        
-        self._set_expert_screen_macro('vertical')
-        self._set_expert_screen_macro('horizontal')
+
+        self._set_expert_screen_macro("vertical")
+        self._set_expert_screen_macro("horizontal")
 
     def _set_expert_screen_macro(self, axis: str) -> None:
         """
@@ -74,20 +77,20 @@ class SmaractTipTiltDouble(SmaractTipTiltDoubleBase):
         motor_pv: str
         button: PyDMRelatedDisplayButton
 
-        if axis not in ['vertical', 'horizontal']:
+        if axis not in ["vertical", "horizontal"]:
             # Don't be silly, silly
             return
 
-        motor_pv = self._get_macro(f'{axis}_motor')
+        motor_pv = self._get_macro(f"{axis}_motor")
 
         if not motor_pv:
-            logger.debug(f'Macro for {axis}_motor does not yet exist')
+            logger.debug(f"Macro for {axis}_motor does not yet exist")
             return
-        
-        button = getattr(self, f'{axis}_expert_screen')
+
+        button = getattr(self, f"{axis}_expert_screen")
         button.setMacros(json.dumps({"motor": motor_pv}))
-        logger.debug(f'Setting {axis} expert screen with macros {button._macros}')  
-    
+        logger.debug(f"Setting {axis} expert screen with macros {button._macros}")
+
     def _invert_axis_channel(self, axis: str) -> None:
         """
         Invert the STEP_FORWARD or STEP_REVERSE channel connections for a particular
@@ -100,33 +103,33 @@ class SmaractTipTiltDouble(SmaractTipTiltDoubleBase):
         """
         checkbox: QCheckBox
         widget: PyDMPushButton
-        
-        if axis not in ['vertical', 'horizontal']:
+
+        if axis not in ["vertical", "horizontal"]:
             # Don't be silly please
             return
 
-        motor_pv = self._get_macro(f'{axis}_motor')
+        motor_pv = self._get_macro(f"{axis}_motor")
 
         if not motor_pv:
-            logger.debug(f'Macro for {axis}_motor does not yet exist')
+            logger.debug(f"Macro for {axis}_motor does not yet exist")
             return
 
-        checkbox = getattr(self, f'{axis}_invert')
-        directions = ['up', 'down'] if axis == 'vertical' else ['right', 'left']
+        checkbox = getattr(self, f"{axis}_invert")
+        directions = ["up", "down"] if axis == "vertical" else ["right", "left"]
 
         # didn't want to use dir/s as an iterator which is normally for directory
         for d in directions:
-            if d in ['up', 'right']:
-                pv_suffix = 'REVERSE' if checkbox.isChecked() else 'FORWARD'
+            if d in ["up", "right"]:
+                pv_suffix = "REVERSE" if checkbox.isChecked() else "FORWARD"
             else:
-                pv_suffix = 'FORWARD' if checkbox.isChecked() else 'REVERSE'
-            widget = getattr(self, f'step_{d}')
-            widget.set_channel(f'ca://{motor_pv}:STEP_{pv_suffix}.PROC')
+                pv_suffix = "FORWARD" if checkbox.isChecked() else "REVERSE"
+            widget = getattr(self, f"step_{d}")
+            widget.set_channel(f"ca://{motor_pv}:STEP_{pv_suffix}.PROC")
 
     def _invert_vertical(self) -> None:
         """Swap the FWD and BWD buttons for the vertical axis"""
-        self._invert_axis_channel('vertical')
-    
+        self._invert_axis_channel("vertical")
+
     def _invert_horizontal(self) -> None:
         """Swap the FWD and BWD buttons for the horizontal axis"""
-        self._invert_axis_channel('horizontal')
+        self._invert_axis_channel("horizontal")
