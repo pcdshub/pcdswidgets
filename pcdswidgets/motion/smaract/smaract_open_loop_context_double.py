42a93012e88432ca4a75cd46e71beebf359201f0
aberges-SLAC <aberges@slac.stanford.edu>
2026-04-24 13:56:00 -0700

    ENH: add Designer Icon for Smaract tip tilt context widgets

--- pcdswidgets/motion/smaract/smaract_open_loop_context_double.py
+++ pcdswidgets/motion/smaract/smaract_open_loop_context_double.py
@@ -5,8 +5,10 @@ This file can be safely edited to change the runtime behavior of the widget.
 """
 
 import logging
+from pathlib import Path
 
 from pydm.widgets import PyDMPushButton
+from qtpy.QtGui import QIcon
 
 from pcdswidgets.builder.designer_options import DesignerOptions
 from pcdswidgets.builder.icon_options import IconOptions
@@ -23,3 +25,8 @@ class SmaractOpenLoopContextDouble(SmaractOpenLoopContextDoubleBase):
         is_container=False,
         icon=IconOptions.NONE,
     )
+
+    @staticmethod
+    def get_designer_icon() -> str:
+        """Icon for usage in Qt designer."""
+        return QIcon(str(Path(__file__).parent / "smaract_tip_tilt_context_qt_icon.png"))
