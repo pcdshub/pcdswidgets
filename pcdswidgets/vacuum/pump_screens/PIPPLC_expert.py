2f8580e0117e5ef3767b87e2de315af6948c5195
Truman Paras <tparas@slac.stanord.edu>
2026-06-22 11:27:22 -0700

    adding PIPPLC screens

--- pcdswidgets/vacuum/pump_screens/PIPPLC_expert.py
+++ pcdswidgets/vacuum/pump_screens/PIPPLC_expert.py
@@ -6,10 +6,10 @@ This file can be safely edited to change the runtime behavior of the widget.
 
 from pcdswidgets.builder.designer_options import DesignerOptions
 from pcdswidgets.builder.icon_options import IconOptions
-from pcdswidgets.generated.vacuum.pump_screens.PIPCombined_base import PipcombinedBase
+from pcdswidgets.generated.vacuum.pump_screens.PIPPLC_expert_base import PipplcExpertBase
 
 
-class Pipcombined(PipcombinedBase):
+class PipplcExpert(PipplcExpertBase):
     designer_options = DesignerOptions(
         group="ECS Vacuum Pump_Screens",
         is_container=False,
