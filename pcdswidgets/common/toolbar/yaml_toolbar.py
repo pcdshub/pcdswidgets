d45cd7ecd451de7374d1dfce76689727bd3875ab
Zachary Lentz <zlentz@slac.stanford.edu>
2026-07-07 11:43:29 -0700

    ENH: toolbar cleanup
    Adjust naming, docstrings, type annotations, function layout
    Add the filename property for basic designer use
    Add to designer
    Adjust set_config_file to not raise as we type filename into designer

--- pcdswidgets/common/toolbar/yaml_toolbar.py
+++ pcdswidgets/common/toolbar/yaml_toolbar.py
@@ -1,57 +1,100 @@
+"""
+A button toolbar loaded from a lucid-style yaml file.
+
+This was originally part of lucid but has moved here so it can be used without
+the lucid launcher, of which it has no real dependencies.
+"""
+
 import logging
 import os
+from typing import Any
 
 import yaml
 from pydm.widgets import PyDMRelatedDisplayButton, PyDMShellCommand
 from qtpy import QtCore, QtWidgets
 from qtpy.QtCore import Qt
-from qtpy.QtWidgets import QGridLayout, QPushButton
+from qtpy.QtWidgets import QGridLayout, QPushButton, QWidget
 
 from pcdswidgets.common.dock.tab_dock_button import TabDockButton
+
+try:
+    from qtpy.QtCore import Property  # type: ignore
+except ImportError:
+    from qtpy.QtCore import pyqtProperty as Property  # type: ignore
 
 logger = logging.getLogger(__name__)
 
 
-class QuickAccessToolbar(QtWidgets.QWidget):
-    """Tab Widget with tabs containing buttons defined via a yaml file"""
+class YamlToolbar(QtWidgets.QWidget):
+    """
+    Tab Widget with tabs containing buttons defined via a yaml file.
 
-    def __init__(self, parent=None):
+    This uses the legacy yaml format from lucid, which defines which buttons go in which tab.
+
+    See https://pcdshub.github.io/lucid/master/toolbar.html for more information.
+    """
+
+    _qt_designer_ = {
+        "group": "ECS Common Toolbar",
+        "is_container": False,
+    }
+
+    def __init__(self, parent: QWidget | None = None):
         super().__init__(parent=parent)
 
-        self._tools_file = ""
-        self._tools = None
+        self._config_file = ""
         self._default_config = {"cols": 4}
         self.default_dock_button = None
-        self._setup_ui()
 
-    def set_tools_file(self, file: str):
-        if not file:
-            return
-        self._tools_file = file
-        with open(file) as tf:
-            self._tools = yaml.full_load(tf)
-        self._assemble_tabs()
-
-    def _setup_ui(self):
         self.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
-
         main_layout = QtWidgets.QVBoxLayout()
         self.setLayout(main_layout)
         self.tab = QtWidgets.QTabWidget()
         main_layout.addWidget(self.tab)
 
-    def _assemble_tabs(self):
-        if self._tools is None:
+    def get_config_file(self) -> str:
+        """Return the last set path to the toolbar config yaml file."""
+        return self._config_file
+
+    def set_config_file(self, file: str):
+        """
+        Set the path to the toolbar config yaml file and create the necessary tabs and buttons.
+
+        Parameters
+        ----------
+        file : str
+            The absolute path to the config file.
+        """
+        if not file:
             return
+        if not os.path.isfile(file):
+            return
+        self._config_file = file
+        with open(file) as tf:
+            full_config = yaml.full_load(tf)
+        self._assemble_tabs(full_config)
+
+    filename = Property("QString", get_config_file, set_config_file)
+
+    def _assemble_tabs(self, full_config: dict[str, dict[str, Any]]):
+        """
+        Build the tabs from the user's yaml config.
+
+        Parameters
+        ----------
+        full_config : dict
+            The serialized configuration from the user's yaml file.
+            Contains configuration information for all tabs.
+        """
         self.tab.clear()
-        for tab_name, tab_params in self._tools.items():
+        for tab_name, tab_params in full_config.items():
             page = QtWidgets.QWidget()
 
             config = dict(self._default_config)
             config.update(tab_params.get("config", {}))
 
             cols = config.get("cols", 4)
-            page.setLayout(SnakeLayout(cols))
+            page.setLayout(YamlTabLayout(cols))
 
             buttons = tab_params.get("buttons", {})
             for button_text, button_config in buttons.items():
@@ -67,9 +110,22 @@ class QuickAccessToolbar(QtWidgets.QWidget):
             scroll_area.minimumSizeHint = min_scroll_size_hint
             self.tab.addTab(scroll_area, tab_name)
 
-    def _button_factory(self, text, config):
+    def _button_factory(self, text: str, config: dict[str, Any]) -> QPushButton:
+        """
+        Create and return a single button for the grid.
+
+        Parameters
+        ----------
+        text : str
+            The text to put on the button.
+        config : dict[str, str]
+            The configuration for this button as specified in the yaml file.
+            This is a dictionary mapping with one special key, "type" that
+            is the button type to use. The valid types are "shell",
+            "display", "dock". All other key/value pairs are property/value
+            pairs to set on the button instance.
+        """
         tp = config.pop("type")
-        btn = QPushButton()
         if tp == "shell":
             btn = PyDMShellCommand()
             btn.showIcon = False
@@ -83,11 +139,13 @@ class QuickAccessToolbar(QtWidgets.QWidget):
             btn.setText(text)
             if self.default_dock_button is None or config.pop("default", False):
                 self.default_dock_button = btn
+        else:
+            raise RuntimeError(f"Invalid button type {tp}")
 
         for prop, val in config.items():
             if prop == "filename":
                 if not os.path.isabs(val):
-                    val = os.path.join(os.path.dirname(self._tools_file), val)
+                    val = os.path.join(os.path.dirname(self._config_file), val)
             try:
                 setattr(btn, prop, val)
             except Exception as ex:
@@ -96,9 +154,9 @@ class QuickAccessToolbar(QtWidgets.QWidget):
         return btn
 
 
-class SnakeLayout(QGridLayout):
+class YamlTabLayout(QGridLayout):
     """
-    Snaking Layout
+    QGridLayout with a simplified addWidget function.
 
     The size is the maximum number of widgets before beginning the next row or
     column. The direction specifies whether the grid pattern will be filled
@@ -106,38 +164,19 @@ class SnakeLayout(QGridLayout):
 
     Parameters
     ----------
-    widgets: Iterable
-        List of widgets to place in grid
-
     size: int
         Maximum size of row or column
 
-    direction: Qt.Direction, optional
+    direction: Qt.Orientation, optional
         Whether the layout is filled column or row first.
-
-    Returns
-    -------
-    QGridLayout
-        Filled with widgets provided in function call
-
-    Example
-    -------
-    .. code:: python
-
-        # Three rows
-        gridify(widgets, 3, direction=Qt.Vertical)
-
-        # Five columns
-        gridify(widgets, 5, direction=Qt.Vertical)  # Default direction
-
     """
 
-    def __init__(self, size, direction=Qt.Horizontal):
+    def __init__(self, size: int, direction: Qt.Orientation = Qt.Horizontal):
         super().__init__()
         self.size = int(size)
         self.direction = direction
 
-    def addWidget(self, widget):  # type: ignore
+    def addWidget(self, widget: QWidget):  # type: ignore
         """Add a QWidget to the layout"""
         # Number of widgets already existing
         position = self.count()
