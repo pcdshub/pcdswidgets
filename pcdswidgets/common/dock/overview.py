b0f675e9cc00fda9b43c4a34eade249f71da98d3
Zachary Lentz <zlentz@slac.stanford.edu>
2026-07-08 15:20:21 -0700

    WIP: add source files from lucid verbatim before tweaking

---
+++ pcdswidgets/common/dock/overview.py
@@ -0,0 +1,393 @@
+"""Overview of the Experimental Area"""
+
+import collections
+import logging
+import os
+from typing import Callable, cast
+
+import yaml
+from pydm.widgets import PyDMRelatedDisplayButton, PyDMShellCommand
+from qtpy import QtCore, QtWidgets
+from qtpy.QtCore import QEvent, QSize, Qt
+from qtpy.QtGui import QHoverEvent, QMouseEvent
+from qtpy.QtWidgets import QGridLayout, QMenu, QPushButton, QWidget
+from typhos.utils import reload_widget_stylesheet
+
+from .dock import LucidDock, LucidDockButton
+from .utils import SnakeLayout, display_for_device, indicator_for_device
+
+try:
+    from qtpy.QtCore import Property  # type: ignore  # noqa: I001
+except ImportError:
+    from qtpy.QtCore import pyqtProperty as Property  # type: ignore  # noqa: I001
+
+logger = logging.getLogger(__name__)
+
+
+class BaseDeviceButton(QPushButton):
+    """Base class for QPushButton to show devices"""
+
+    _OPEN_ALL = "Open All"
+
+    devices: list
+
+    def __init__(self, title, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title = title
+        # References for created screens
+        self._device_displays: dict[str, QWidget] = {}
+        self._suite = None
+        # Setup Menu
+        self.setContextMenuPolicy(Qt.PreventContextMenu)
+        self.device_menu = QMenuWithClickableSubmenu()
+        self.device_menu.aboutToShow.connect(self._menu_shown)
+
+    def show_device(self, device):
+        if device.name not in self._device_displays:
+            widget = display_for_device(device)
+            widget.setParent(self)
+            self._device_displays[device.name] = widget
+        return self._device_displays[device.name]
+
+    def show_all(self):
+        """Create a widget for contained devices"""
+        return [self.show_device(device=device) for device in self.devices]
+
+    def get_all_titles(self):
+        """Get the titles of each window, e.g. the name of each device."""
+        return [device.name for device in self.devices]
+
+    def _devices_shown(self, shown):
+        """Implemeted by subclass"""
+        pass
+
+    def _menu_shown(self):
+        # Current menu options
+        menu_devices = [action.text() for action in self.device_menu.actions()]
+        if self._OPEN_ALL not in menu_devices:
+            sub_menu = self.device_menu.addMenu(self._OPEN_ALL)
+            LucidDock.add_many_to_dock_user_menu(
+                widget_list=self.show_all,
+                title_list=self.get_all_titles,
+                menu=sub_menu,
+            )
+            self.device_menu.addMenu(sub_menu)
+            self.device_menu.addSeparator()
+        # Add devices
+        for device in self.devices:
+            if device.name not in menu_devices:
+                # Add to device menu
+                show_device = self._show_device_wrapper(device)
+                self._add_to_menu(widget_func=show_device, text=device.name)
+
+    def _add_to_menu(self, widget_func: Callable[[], QWidget], text: str):
+        sub_menu = self.device_menu.addMenu(text)
+        LucidDock.add_to_dock_user_menu(widget=widget_func, title=text, menu=sub_menu)
+        sub_menu.setDefaultAction(sub_menu.actions()[0])
+        self.device_menu.addMenu(sub_menu)
+
+    def _show_device_wrapper(self, device):
+        def inner():
+            return self.show_device(device)
+
+        return inner
+
+    def eventFilter(self, obj, event):  # type: ignore
+        """
+        QWidget.eventFilter to be installed on child indicators
+
+        This is required to display the :meth:`.contextMenuEvent` even if an
+        indicator is pressed.
+        """
+        # Filter child widgets events to show context menu
+        if event.type() == QEvent.MouseButtonPress:
+            event = cast(QMouseEvent, event)
+            if len(self.devices) == 0:
+                # Important: don't do anything when there are no devices
+                return False
+            elif len(self.devices) == 1:
+                device = self.devices[0]
+                deferred_widget = self._show_device_wrapper(self.devices[0])
+                if event.button() == Qt.LeftButton:
+                    LucidDock.add_to_dock_user_keybinds(widget=deferred_widget, title=device.name)
+                    return True
+                elif event.button() == Qt.RightButton:
+                    LucidDock.add_to_dock_user_menu(widget=deferred_widget, title=device.name, pos=event.globalPos())
+                    return True
+            elif event.button() in (Qt.LeftButton, Qt.RightButton):
+                self.device_menu.exec_(event.globalPos())
+                return True
+        return False
+
+
+class QMenuWithClickableSubmenu(QMenu):
+    """
+    QMenu, but we can click our submenus to do their default actions.
+    """
+
+    def mousePressEvent(self, event: QMouseEvent) -> None:  # type: ignore
+        if event.button() == Qt.LeftButton:
+            action = self.actionAt(event.pos())
+            if action is None:
+                return super().mousePressEvent(event)
+            submenu_default_action = action.menu().defaultAction()
+            if submenu_default_action is None:
+                return super().mousePressEvent(event)
+            submenu_default_action.trigger()
+            self.close()
+        else:
+            return super().mousePressEvent(event)
+
+
+class IndicatorCell(BaseDeviceButton):
+    """Single Cell of Indicator Lights in the Overview Grid"""
+
+    max_columns = 5
+    icon_size = 12
+    spacing = 1
+    margin = 5
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # Disable borders on the widget unless a hover occurs
+        self.setStyleSheet("QPushButton:!hover {border: None}")
+        self.setLayout(SnakeLayout(self.max_columns))
+        self.layout().setSpacing(self.spacing)
+        self.layout().setContentsMargins(*4 * [self.margin])
+        self._selecting_widgets = []
+        self.installEventFilter(self)
+        self.devices = []
+
+    @property
+    def matchable_names(self):
+        """All names used for text searching"""
+        return [self.title] + [device.name for device in self.devices]
+
+    @Property(bool)  # type: ignore
+    def selected(self) -> bool:
+        """Whether the devices in this cell have been selected"""
+        return bool(len(self._selecting_widgets))
+
+    def add_indicator(self, widget):
+        """Add an indicator to the Panel"""
+        widget.setFixedSize(self.icon_size, self.icon_size)
+        widget.setMinimumSize(self.icon_size, self.icon_size)
+        self.layout().addWidget(widget)
+
+    def add_device(self, device):
+        """Add a device to the IndicatorCell"""
+        indicator = indicator_for_device(device)
+        indicator.setContextMenuPolicy(Qt.NoContextMenu)
+        self.devices.append(device)
+        self.add_indicator(indicator)
+
+    def sizeHint(self):
+        size_per_icon = self.icon_size + self.spacing
+        return QSize(self.max_columns * size_per_icon + self.spacing + 2 * self.margin, 36)
+
+    def _devices_shown(self, shown, selector=None):
+        """Callback when corresponding ``TyphosSuite`` is accessed"""
+        selector = selector or self
+        # On first selection
+        if shown and selector not in self._selecting_widgets:
+            self._selecting_widgets.append(selector)
+            reload_widget_stylesheet(self)
+        # On closure
+        elif not shown and selector in self._selecting_widgets:
+            self._selecting_widgets.remove(selector)
+            reload_widget_stylesheet(self)
+
+
+class IndicatorGroup(BaseDeviceButton):
+    """QPushButton to select an entire row or column of devices"""
+
+    def __init__(self, *args, orientation, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.setText(str(self.title))
+        self.cells = []
+        self.installEventFilter(self)
+        self.orientation = orientation
+
+    def add_cell(self, cell):
+        self.cells.append(cell)
+
+    @property
+    def devices(self) -> list:  # type: ignore
+        """All devices contained in the ``IndicatorGroup``"""
+        return [device for cell in self.cells for device in cell.devices]
+
+    @property
+    def device_to_indicator(self):
+        """Dictionary of Device to IndicatorCell"""
+        return {device: cell for cell in self.cells for device in cell.devices}
+
+    def eventFilter(self, obj, event):  # type: ignore
+        """Share QHoverEvents with all cells in the group"""
+        if isinstance(event, QHoverEvent):
+            for cell in self.cells:
+                cell.event(event)
+                return False
+        return super().eventFilter(obj, event)
+
+    def _devices_shown(self, shown):
+        """Selecting this button, selects all contained cells"""
+        for cell in self.cells:
+            cell._devices_shown(shown, selector=self)
+
+
+class IndicatorGrid(QWidget):
+    """GridLayout of all Indicators"""
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+        self.grid = QGridLayout()
+        self.setLayout(self.grid)
+        self.grid.setSpacing(0)
+        self.grid.setSizeConstraint(QGridLayout.SetFixedSize)
+        self._groups = {}
+        self.setStyleSheet(
+            """\
+QWidget[selected="true"] {background-color: rgba(20, 140, 210, 150);}
+            """
+        )
+
+    @property
+    def groups(self):
+        "A dictionary of name to IndicatorGroup"
+        return dict(self._groups)
+
+    def add_devices(self, devices, system=None, stand=None):
+        # Create cell
+        cell = IndicatorCell(title=f"{stand} {system}")
+        for device in devices:
+            cell.add_device(device)
+        # Add to proper location in grid
+        coords = []
+        for i, group_name in enumerate((system, stand)):
+            # Create the group if not present
+            if group_name not in self._groups:
+                self._add_group(group_name, bool(i))
+            # Add cell to group
+            # Coordinate of group
+            group = self._groups[group_name]
+            idx = self.grid.indexOf(group)
+            coords.append(self.grid.getItemPosition(idx)[i])
+            if cell:
+                group.add_cell(cell)
+        # Add cell to correct location in grid
+        if cell:
+            self.grid.addWidget(cell, coords[0], coords[1], Qt.AlignTop)
+
+    def _add_group(self, group, as_row):
+        # Add to layout
+        group = IndicatorGroup(title=group, orientation="row" if as_row else "column")
+        self._groups[group.title] = group
+        # Find the correct position
+        if as_row:
+            (row, column) = (0, self.grid.columnCount())
+        else:
+            (row, column) = (self.grid.rowCount(), 0)
+        self.grid.addWidget(group, row, column, Qt.AlignVCenter)
+
+    def add_from_dict(self, devices=None):
+        rows = set()
+        cols = set()
+        if devices is None:
+            return
+        for e in devices:
+            r, c = e.split("|")
+            rows.add(r)
+            cols.add(c)
+
+        data = collections.OrderedDict()
+        for r in sorted(rows):
+            for c in sorted(cols):
+                data[f"{r}|{c}"] = devices.get(f"{r}|{c}") or []
+
+        for location, dev_list in data.items():
+            stand, system = location.split("|")
+            self.add_devices(dev_list, stand=stand, system=system)
+
+
+class QuickAccessToolbar(QtWidgets.QWidget):
+    """Tab Widget with tabs containing buttons defined via a yaml file"""
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self._tools_file = ""
+        self._tools = None
+        self._default_config = {"cols": 4}
+        self.default_dock_button = None
+        self._setup_ui()
+
+    def set_tools_file(self, file: str):
+        if not file:
+            return
+        self._tools_file = file
+        with open(file) as tf:
+            self._tools = yaml.full_load(tf)
+        self._assemble_tabs()
+
+    def _setup_ui(self):
+        self.setSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+
+        main_layout = QtWidgets.QVBoxLayout()
+        self.setLayout(main_layout)
+        self.tab = QtWidgets.QTabWidget()
+        main_layout.addWidget(self.tab)
+
+    def _assemble_tabs(self):
+        if self._tools is None:
+            return
+        self.tab.clear()
+        for tab_name, tab_params in self._tools.items():
+            page = QtWidgets.QWidget()
+
+            config = dict(self._default_config)
+            config.update(tab_params.get("config", {}))
+
+            cols = config.get("cols", 4)
+            page.setLayout(SnakeLayout(cols))
+
+            buttons = tab_params.get("buttons", {})
+            for button_text, button_config in buttons.items():
+                button_widget = self._button_factory(button_text, button_config)
+                page.layout().addWidget(button_widget)
+
+            def min_scroll_size_hint(*args, **kwargs):
+                return QtCore.QSize(40, 40)
+
+            scroll_area = QtWidgets.QScrollArea()
+            scroll_area.setWidgetResizable(True)
+            scroll_area.setWidget(page)
+            scroll_area.minimumSizeHint = min_scroll_size_hint
+            self.tab.addTab(scroll_area, tab_name)
+
+    def _button_factory(self, text, config):
+        tp = config.pop("type")
+        btn = QPushButton()
+        if tp == "shell":
+            btn = PyDMShellCommand()
+            btn.showIcon = False
+            btn.setText(text)
+        elif tp == "display":
+            btn = PyDMRelatedDisplayButton()
+            btn.showIcon = False
+            btn.setText(text)
+        elif tp == "dock":
+            btn = LucidDockButton()
+            btn.setText(text)
+            if self.default_dock_button is None or config.pop("default", False):
+                self.default_dock_button = btn
+
+        for prop, val in config.items():
+            if prop == "filename":
+                if not os.path.isabs(val):
+                    val = os.path.join(os.path.dirname(self._tools_file), val)
+            try:
+                setattr(btn, prop, val)
+            except Exception as ex:
+                logger.error(f"Failed to set property {prop} with value {val} for {tp}: {ex}")
+
+        return btn
