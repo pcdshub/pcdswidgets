3e3496eeb01d0f39f332e503ade91bc677479e4a
Zachary Lentz <zlentz@slac.stanford.edu>
2026-07-08 17:45:03 -0700

    MNT/DOC: as much cleanup as I could be motivated to do today

--- pcdswidgets/common/dock/indicator_grid.py
+++ pcdswidgets/common/dock/indicator_grid.py
@@ -1,9 +1,22 @@
-"""Overview of the Experimental Area"""
+"""
+Grid of indicators representing devices in the experiment area.
+
+This requires the optional "happi" and "typhos" dependencies to function at runtime,
+but can be placed in designer without these installed.
+It will also require any user modules used to define device types in the "happi"
+database, such as "ophyd" and/or "pcdsdevices".
+
+If a TabDock exists in the application, the grid will open "typhos" screens
+in the dock, otherwise they will pop out as floating windows.
+The standard keyboard shortcuts and right click options for dock windows
+are supported.
+"""
 
 import collections
 import functools
 import logging
-from typing import Callable, cast
+import textwrap
+from typing import Any, Callable, Iterable, Protocol, cast
 
 from pydm.utilities import is_qt_designer
 from qtpy import QtCore
@@ -23,16 +36,44 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
+class Device(Protocol):
+    """Type annotation stand-in for an ophyd Device (optional import)."""
+
+    name: str
+
+
+class Entry(Protocol):
+    """Type annotation stand-in for a happi Entry (optional import)."""
+
+    metadata: dict[str, Any]
+
+
+# Type alias for the argument type required for callbacks in HappiLoader
+HappiLoaderCbDict = dict[str, list[Device]]
+
+
+class HappiLoaderCallback(Protocol):
+    """Type annotation helper for HappiLoader-compatible callback functions."""
+
+    def __call__(self, devices: HappiLoaderCbDict) -> None: ...
+
+
 class BaseDeviceButton(QPushButton):
-    """Base class for QPushButton to show devices"""
+    """
+    Base class for QPushButton to show devices.
+
+    Child classes should populate the "devices" attribute, which is a list
+    that contains all the typhos-compatible device objects to include in the button.
+
+    All init parameters are passed directly through to QPushButton.
+    """
 
     _OPEN_ALL = "Open All"
 
-    devices: list
+    devices: list[Device]
 
-    def __init__(self, title, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.title = title
         # References for created screens
         self._device_displays: dict[str, QWidget] = {}
         self._suite = None
@@ -41,26 +82,24 @@ class BaseDeviceButton(QPushButton):
         self.device_menu = QMenuWithClickableSubmenu()
         self.device_menu.aboutToShow.connect(self._menu_shown)
 
-    def show_device(self, device):
+    def show_device(self, device: Device) -> QWidget:
+        """Return the typhos display for a device, creating it if necessary."""
         if device.name not in self._device_displays:
             widget = display_for_device(device)
             widget.setParent(self)
             self._device_displays[device.name] = widget
         return self._device_displays[device.name]
 
-    def show_all(self):
-        """Create a widget for contained devices"""
+    def show_all(self) -> list[QWidget]:
+        """Return the typhos displays for all devices, creating them if necessary."""
         return [self.show_device(device=device) for device in self.devices]
 
     def get_all_titles(self):
-        """Get the titles of each window, e.g. the name of each device."""
+        """Return the titles of each window, e.g. the name of each device."""
         return [device.name for device in self.devices]
 
-    def _devices_shown(self, shown):
-        """Implemeted by subclass"""
-        pass
-
     def _menu_shown(self):
+        """Update the right-click context menu when we need to show it."""
         # Current menu options
         menu_devices = [action.text() for action in self.device_menu.actions()]
         if self._OPEN_ALL not in menu_devices:
@@ -80,12 +119,15 @@ class BaseDeviceButton(QPushButton):
                 self._add_to_menu(widget_func=show_device, text=device.name)
 
     def _add_to_menu(self, widget_func: Callable[[], QWidget], text: str):
+        """Add a single device option to the menu."""
         sub_menu = self.device_menu.addMenu(text)
         TabDock.add_to_dock_user_menu(widget=widget_func, title=text, menu=sub_menu)
         sub_menu.setDefaultAction(sub_menu.actions()[0])
         self.device_menu.addMenu(sub_menu)
 
-    def _show_device_wrapper(self, device):
+    def _show_device_wrapper(self, device: Device) -> Callable[[], QWidget]:
+        """Helper for assembling the menus."""
+
         def inner():
             return self.show_device(device)
 
@@ -139,7 +181,14 @@ class QMenuWithClickableSubmenu(QMenu):
 
 
 class IndicatorCell(BaseDeviceButton):
-    """Single Cell of Indicator Lights in the Overview Grid"""
+    """
+    Single Cell of Indicator Lights in the IndicatorGrid.
+
+    In the grid, these are placed in the interior area (not the edges).
+    This defines logic for including the circular indicators on the device buttons.
+
+    All init args are passed through to BaseDeviceButton.
+    """
 
     max_columns = 5
     icon_size = 12
@@ -157,23 +206,18 @@ class IndicatorCell(BaseDeviceButton):
         self.installEventFilter(self)
         self.devices = []
 
-    @property
-    def matchable_names(self):
-        """All names used for text searching"""
-        return [self.title] + [device.name for device in self.devices]
-
     @Property(bool)  # type: ignore
     def selected(self) -> bool:
         """Whether the devices in this cell have been selected"""
         return bool(len(self._selecting_widgets))
 
-    def add_indicator(self, widget):
+    def add_indicator(self, widget: QWidget):
         """Add an indicator to the Panel"""
         widget.setFixedSize(self.icon_size, self.icon_size)
         widget.setMinimumSize(self.icon_size, self.icon_size)
         self.layout().addWidget(widget)
 
-    def add_device(self, device):
+    def add_device(self, device: Device):
         """Add a device to the IndicatorCell"""
         indicator = indicator_for_device(device)
         indicator.setContextMenuPolicy(Qt.NoContextMenu)
@@ -184,41 +228,32 @@ class IndicatorCell(BaseDeviceButton):
         size_per_icon = self.icon_size + self.spacing
         return QSize(self.max_columns * size_per_icon + self.spacing + 2 * self.margin, 36)
 
-    def _devices_shown(self, shown, selector=None):
-        """Callback when corresponding ``TyphosSuite`` is accessed"""
-        import typhos.utils
-
-        selector = selector or self
-        # On first selection
-        if shown and selector not in self._selecting_widgets:
-            self._selecting_widgets.append(selector)
-            typhos.utils.reload_widget_stylesheet(self)
-        # On closure
-        elif not shown and selector in self._selecting_widgets:
-            self._selecting_widgets.remove(selector)
-            typhos.utils.reload_widget_stylesheet(self)
-
 
 class IndicatorGroup(BaseDeviceButton):
-    """QPushButton to select an entire row or column of devices"""
+    """
+    QPushButton to select an entire row or column of devices.
 
-    def __init__(self, *args, orientation, **kwargs):
+    In the grid, these are placed at the row and column headers.
+    """
+
+    def __init__(self, title: str, *args, orientation: str, **kwargs):
         super().__init__(*args, **kwargs)
+        self.title = title
         self.setText(str(self.title))
-        self.cells = []
+        self.cells: list[IndicatorCell] = []
         self.installEventFilter(self)
         self.orientation = orientation
 
-    def add_cell(self, cell):
+    def add_cell(self, cell: IndicatorCell):
         self.cells.append(cell)
 
     @property
-    def devices(self) -> list:  # type: ignore
+    def devices(self) -> list[Device]:  # type: ignore
         """All devices contained in the ``IndicatorGroup``"""
         return [device for cell in self.cells for device in cell.devices]
 
     @property
-    def device_to_indicator(self):
+    def device_to_indicator(self) -> dict[Device, IndicatorCell]:
         """Dictionary of Device to IndicatorCell"""
         return {device: cell for cell in self.cells for device in cell.devices}
 
@@ -230,52 +265,56 @@ class IndicatorGroup(BaseDeviceButton):
                 return False
         return super().eventFilter(obj, event)
 
-    def _devices_shown(self, shown):
-        """Selecting this button, selects all contained cells"""
-        for cell in self.cells:
-            cell._devices_shown(shown, selector=self)
-
 
 class IndicatorGrid(QWidget):
-    """GridLayout of all Indicators"""
+    """
+    Grid of all Indicators.
+
+    This is the widget that goes in the screen.
+    It can be parameterized only by setting the "beamline" property,
+    which should be a valid "beamline" from happi, e.g. "TMO", "XPP", etc.
+
+    This will search happi for every "active" device marked on that "beamline"
+    and sort them into the grid by their "functional_group" and "location_group" keys.
+    """
 
     _qt_designer_ = {
         "group": "ECS Common Dock",
         "is_container": False,
     }
 
-    def __init__(self, parent=None):
+    def __init__(self, parent: QWidget | None = None):
         super().__init__(parent=parent)
         self._beamline = ""
         self.grid = QGridLayout()
         self.setLayout(self.grid)
         self.grid.setSpacing(0)
         self.grid.setSizeConstraint(QGridLayout.SetFixedSize)
-        self._groups = {}
+        self._groups: dict[str, IndicatorGroup] = {}
         self.setStyleSheet(
-            """\
-QWidget[selected="true"] {background-color: rgba(20, 140, 210, 150);}
-            """
+            textwrap.dedent(
+                """
+                QWidget[selected="true"] {background-color: rgba(20, 140, 210, 150);}
+                """
+            )
         )
 
     def set_beamline(self, beamline: str):
+        """Set the beamline parameter, and in a live screen also load the devices from happi."""
         self._beamline = beamline
         if not is_qt_designer():
             self.load_happi(beamline)
 
     def get_beamline(self) -> str:
+        """Return the beamline we've set."""
         return self._beamline
 
     beamline = Property("QString", get_beamline, set_beamline)
 
-    @property
-    def groups(self):
-        "A dictionary of name to IndicatorGroup"
-        return dict(self._groups)
-
-    def add_devices(self, devices, system=None, stand=None):
+    def add_devices(self, devices: list[Device], system: str, stand: str):
+        """Add many devices for a specific system/stand group to the grid."""
         # Create cell
-        cell = IndicatorCell(title=f"{stand} {system}")
+        cell = IndicatorCell()
         for device in devices:
             cell.add_device(device)
         # Add to proper location in grid
@@ -295,9 +334,10 @@ QWidget[selected="true"] {background-color: rgba(20, 140, 210, 150);}
         if cell:
             self.grid.addWidget(cell, coords[0], coords[1], Qt.AlignTop)
 
-    def _add_group(self, group, as_row):
+    def _add_group(self, group_name: str, as_row: bool):
+        """Create a new IndicatorGroup and add it to the grid."""
         # Add to layout
-        group = IndicatorGroup(title=group, orientation="row" if as_row else "column")
+        group = IndicatorGroup(title=group_name, orientation="row" if as_row else "column")
         self._groups[group.title] = group
         # Find the correct position
         if as_row:
@@ -306,7 +346,8 @@ QWidget[selected="true"] {background-color: rgba(20, 140, 210, 150);}
             (row, column) = (self.grid.rowCount(), 0)
         self.grid.addWidget(group, row, column, Qt.AlignVCenter)
 
-    def add_from_dict(self, devices=None):
+    def add_from_dict(self, devices: HappiLoaderCbDict):
+        """Add devices from the dict presented to HappiLoader callbacks."""
         rows = set()
         cols = set()
         if devices is None:
@@ -326,13 +367,14 @@ QWidget[selected="true"] {background-color: rgba(20, 140, 210, 150);}
             self.add_devices(dev_list, stand=stand, system=system)
 
     def load_happi(self, beamline: str):
+        """Load happi devices into the grid using a background thread."""
         self.loader = HappiLoader(
             beamline=[beamline], group_keys=("location_group", "functional_group"), callbacks=[self.add_from_dict]
         )
         self.loader.start()
 
 
-def get_happi_entry_value(entry, key):
+def get_happi_entry_value(entry: Entry, key: str) -> Any:
     value = entry.metadata.get(key, None)
     if not value:
         raise ValueError(f"Invalid Key ({key} not in {entry}.")
@@ -340,13 +382,22 @@ def get_happi_entry_value(entry, key):
 
 
 class HappiLoader(QtCore.QThread):
-    def __init__(self, *args, beamline, group_keys, callbacks, **kwargs):
+    """Thread for loading happi devices in the background."""
+
+    def __init__(
+        self,
+        *args,
+        beamline: Iterable[str],
+        group_keys: tuple[str, str],
+        callbacks: Iterable[HappiLoaderCallback],
+        **kwargs,
+    ):
         self.beamline = beamline
         self.group_keys = group_keys
         self.callbacks = callbacks
         super().__init__(*args, **kwargs)
 
-    def _load_from_happi(self, row_group_key, col_group_key):
+    def _load_from_happi(self, row_group_key: str, col_group_key: str) -> HappiLoaderCbDict:
         """Fill with Data from Happi"""
         cli = get_happi_client()
         results = []
@@ -373,6 +424,7 @@ class HappiLoader(QtCore.QThread):
         return dev_groups
 
     def run(self):
+        """When we start the thread, this runs in the background to load from happi and run callbacks."""
         row_group_key, col_group_key = self.group_keys
 
         dev_groups = self._load_from_happi(row_group_key, col_group_key)
@@ -386,19 +438,20 @@ class HappiLoader(QtCore.QThread):
 device_display_cache = {}
 
 
-def display_for_device(device):
+def display_for_device(device: Device):
     """Create a TyphosDeviceDisplay for a given device"""
-    import typhos.display
-    import typhos.utils
+    # Embed optional dependency imports in function call
+    from typhos.display import TyphosDeviceDisplay
+    from typhos.utils import apply_standard_stylesheets, no_device_lazy_load
 
     try:
         return device_display_cache[device]
     except KeyError:
         ...
-    with typhos.utils.no_device_lazy_load():
+    with no_device_lazy_load():
         logger.debug("Creating device display for %r", device)
-        display = typhos.display.TyphosDeviceDisplay.from_device(device, scroll_option="scrollbar")
-        typhos.utils.apply_standard_stylesheets(widget=display)
+        display = TyphosDeviceDisplay.from_device(device, scroll_option="scrollbar")
+        apply_standard_stylesheets(widget=display)
     device_display_cache[device] = display
     return display
 
@@ -407,10 +460,9 @@ _HAPPI_CLIENT = None
 
 
 def get_happi_client():
-    """
-    Create and cache a happi client from configuration
-    """
+    """Create and cache a happi client from configuration."""
     global _HAPPI_CLIENT
+    # Embed optional dependency imports in function call
     import happi
 
     if _HAPPI_CLIENT is None:
@@ -419,17 +471,18 @@ def get_happi_client():
 
 
 def indicator_for_device(device):  # type: ignore
-    """Create a QWidget to indicate the alarm state of a QWidget"""
-    import typhos.alarm
+    """Create a QWidget to indicate the alarm state of a QWidget."""
+    # Embed optional dependency imports in function call
+    from typhos.alarm import TyphosAlarmCircle
 
     try:
         hints = device.hints["fields"]
     except (AttributeError, KeyError):
         hints = []
-    circle = typhos.alarm.TyphosAlarmCircle()  # type: ignore
+    circle = TyphosAlarmCircle()  # type: ignore
     if hints:
-        circle.kindLevel = typhos.alarm.TyphosAlarmCircle.KindLevel.HINTED  # type: ignore
+        circle.kindLevel = TyphosAlarmCircle.KindLevel.HINTED  # type: ignore
     else:
-        circle.kindLevel = typhos.alarm.TyphosAlarmCircle.KindLevel.NORMAL  # type: ignore
+        circle.kindLevel = TyphosAlarmCircle.KindLevel.NORMAL  # type: ignore
     circle.add_device(device)
     return circle
